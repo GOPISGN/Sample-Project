2112a6a179fd0bb5e1a87bdd88c2aac677bbcc42
GOPISGN <48788981+GOPISGN@users.noreply.github.com>
2020-06-03 12:27:36 +0530

    Create Sample.py

---
+++ Sample.py
@@ -0,0 +1,15 @@
+import sys
+
+if int(sys.argv[1]) < 0:
+    raise ValueError('Please provide a positive number for printing Hello World.')
+    
+print(f'Script Name is {sys.argv[0]}')
+
+num1 = int(sys.argv[1])
+
+i = 1
+
+while i <= num1:
+    print(f'Hello World... {i}')
+    
+    i+=1
